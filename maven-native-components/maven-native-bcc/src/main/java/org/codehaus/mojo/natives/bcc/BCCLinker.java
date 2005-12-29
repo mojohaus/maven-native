10442d9022042a450f5fe15aa5ac5c24683d70aa
Dan Tran <dantran@gmail.com>
2005-12-29 23:03:04 +0000

    add BCCLinker and BCCEnvLinker
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/maven-native@1120 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ maven-native-components/maven-native-bcc/src/main/java/org/codehaus/mojo/natives/bcc/BCCLinker.java
@@ -0,0 +1,124 @@
+package org.codehaus.mojo.natives.bcc;
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2004, The Codehaus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
+import org.codehaus.mojo.natives.NativeBuildException;
+import org.codehaus.mojo.natives.c.CLinker;
+import org.codehaus.mojo.natives.linker.LinkerConfiguration;
+
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.cli.Commandline;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:dantran@gmail.com">Dan Tran</a>
+ * @version $Id$
+ */
+public class BCCLinker 
+    extends CLinker
+{
+	
+    public static final String DEFAULT_EXECUTABLE = "tlib";
+    
+    public BCCLinker ()
+    {
+    }
+	
+    protected Commandline createLinkerCommandLine( List objectFiles, LinkerConfiguration config )
+        throws NativeBuildException
+    {
+        Commandline cl = new Commandline();
+    
+        cl.setWorkingDirectory( config.getWorkingDirectory().getPath() );
+
+        String executable = DEFAULT_EXECUTABLE;
+    
+        /**
+         *Turbo Incremental Link 5.68 Copyright (c) 1997-2005 Borland
+         *Syntax: ILINK32 options objfiles, exefile, mapfile, libfiles, deffile, resfiles        
+         * 
+         */
+        if ( config.getExecutable() != null && config.getExecutable().trim().length() != 0 )
+        {
+            executable = config.getExecutable();
+        }
+    
+        cl.createArgument().setValue( executable );
+    
+        cl.addArguments( config.getStartOptions() );
+        
+        //objfiles
+        for ( int i = 0; i < objectFiles.size(); ++i )
+        {
+            File objFile = (File) objectFiles.get(i);
+
+            cl.createArgument().setValue( " " + objFile.getPath() );
+        }
+
+        //ouput file
+        cl.createArgument().setValue( "," + config.getOutputFilePath() );
+
+        //map files
+        cl.createArgument().setValue( "," );
+        cl.addArguments( config.getMiddleOptions() );
+
+        //setup external libraries in dependencies
+        cl.createArgument().setValue( "," );
+
+        File [] externalLibs = config.getExternalLibraries();
+        
+        for ( int i = 0; i < externalLibs.length; ++i )
+        {
+            if ( ! FileUtils.getExtension( externalLibs[i].getPath() ).toLowerCase().equals( "res" ) )
+            {
+                cl.createArgument().setValue( externalLibs[i].getPath() );
+            }
+        }
+
+        //the rest is linker end options
+        cl.createArgument().setValue( "," );
+        cl.addArguments( config.getEndOptions() );
+        
+        //res file
+        for ( int i = 0; i < externalLibs.length; ++i )
+        {
+            if ( FileUtils.getExtension( externalLibs[i].getPath() ).toLowerCase().equals( "res" ) )
+            {
+                cl.createArgument().setValue( externalLibs[i].getPath() );
+            }
+        }
+        
+        return cl;
+    }
+
+	
+    protected void setupCommandLineEnv( File msvcHome, Commandline cl )
+        throws NativeBuildException
+    {
+        // user must setup their env accourding compiler provided setup script
+    }	
+}
