3a11ffdb06c188ed5b29381f9423dae8c4745daf
Dan Tran <dantran@gmail.com>
2006-01-16 23:30:09 +0000

    add java test
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/maven-native@1302 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ native-maven-plugin/src/it/jni/native/src/test/java/HelloWorldTest.java
@@ -0,0 +1,33 @@
+
+import junit.framework.*;
+
+public class HelloWorldTest
+    extends TestCase {
+
+    public HelloWorldTest(String name)
+    {
+      super(name);
+    }
+
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+    }
+
+    protected void tearDown()
+        throws Exception
+    {
+        super.tearDown();
+    }
+
+    public void testNativeHelloWorld()
+        throws Exception
+    {
+        HelloWorld app = new HelloWorld();
+        app.sayHello();
+    }
+
+
+}
+
