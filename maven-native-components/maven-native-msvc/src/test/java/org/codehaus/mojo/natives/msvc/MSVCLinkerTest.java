ca1fa82763b53126cd00c84679a6b70cb775ad12
Dan Tran <dantran@gmail.com>
2006-09-17 22:16:19 +0000

    ad msvc compiler and linker tests
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/maven-native@2370 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ maven-native-components/maven-native-msvc/src/test/java/org/codehaus/mojo/natives/msvc/MSVCLinkerTest.java
@@ -0,0 +1,75 @@
+package org.codehaus.mojo.natives.msvc;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.mojo.natives.NativeBuildException;
+import org.codehaus.mojo.natives.c.CLinker;
+import org.codehaus.mojo.natives.compiler.CompilerConfiguration;
+import org.codehaus.mojo.natives.linker.LinkerConfiguration;
+import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.util.cli.Commandline;
+
+public class MSVCLinkerTest
+    extends PlexusTestCase
+{
+    private MSVCLinker linker;
+
+    private LinkerConfiguration config;
+
+    private static final File objectFile0 = new File( "source1.obj" );
+
+    private static final File objectFile1 = new File( "source2.obj" );
+
+    private List defautlObjectFiles;
+
+    private String basedir;
+
+    public void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        this.defautlObjectFiles = new ArrayList();
+        this.defautlObjectFiles.add( objectFile0 );
+        this.defautlObjectFiles.add( objectFile1 );
+
+        this.linker = new MSVCLinker();
+        this.config = new LinkerConfiguration();
+        this.basedir = getBasedir();
+        config.setWorkingDirectory( new File( basedir ) );
+        config.setOutputDirectory( new File( basedir, "target" ) );
+        config.setOutputFileExtension( "exe" );
+        config.setOutputFileName( "test" );
+    }
+
+    public void testDefaultLinkerExecutable()
+        throws Exception
+    {
+        Commandline cl = this.getCommandline();
+
+        assertEquals( "link.exe", cl.getExecutable() );
+
+        assertEquals( basedir, cl.getWorkingDirectory().getPath() );
+
+        System.out.println( cl.toString() );
+    }
+
+    public void testSimpleLinkerCommand()
+        throws Exception
+    {
+        Commandline cl = this.getCommandline();
+
+        assertEquals( "link.exe /out:" + config.getOutputFilePath() + " source1.obj source2.obj", cl.toString() );
+    }
+
+    /////////////////////////// HELPERS //////////////////////////////////////
+    private Commandline getCommandline()
+        throws NativeBuildException
+    {
+        return this.linker.createLinkerCommandLine( defautlObjectFiles, config );
+    }
+
+ 
+}
