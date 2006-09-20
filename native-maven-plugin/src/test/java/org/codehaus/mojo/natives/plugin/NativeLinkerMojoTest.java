9f862cf5983bd9b753a634aeb0d1d36598332eb6
Dan Tran <dantran@gmail.com>
2006-09-20 15:52:32 +0000

    add another dummy test for Link Mojo
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/maven-native@2383 52ab4f32-60fc-0310-b215-8acea882cd1b

--- native-maven-plugin/src/test/java/org/codehaus/mojo/natives/plugin/NativeLinkerMojoTest.java
+++ native-maven-plugin/src/test/java/org/codehaus/mojo/natives/plugin/NativeLinkerMojoTest.java
@@ -1,7 +1,16 @@
 package org.codehaus.mojo.natives.plugin;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.artifact.handler.DefaultArtifactHandler;
+import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 
 public class NativeLinkerMojoTest
@@ -15,4 +24,31 @@ public class NativeLinkerMojoTest
         assertNotNull( mojo );
     }
 
+    public void NotestExecute()
+        throws Exception
+    {
+        File pluginXml = new File( getBasedir(), "src/test/resources/linker/plugin-config.xml" );
+        NativeLinkMojo mojo = (NativeLinkMojo) lookupMojo( "link", pluginXml );
+        assertNotNull( mojo );
+        
+        //must init this
+        mojo.setPluginContext( new HashMap() );
+        
+        //simulate object files
+        List objectList = new ArrayList();
+        objectList.add( new File( "o1.o" ) );
+        objectList.add( new File( "o2.o" ) );
+        mojo.saveCompilerOutputFilePaths( objectList );
+        
+        //simulate artifact 
+        ArtifactHandler artifactHandler = new DefaultArtifactHandler();
+        Artifact artifact = new DefaultArtifact( "test", "test", VersionRange.createFromVersion( "1.0-SNAPSHOT" ), "compile", "exe", null, artifactHandler );
+        mojo.getProject().setArtifact( artifact );
+
+        //simulate artifacts 
+        mojo.getProject().setArtifacts( new HashSet() ); //no extern libs for now
+        
+        mojo.execute();
+         
+    }
 }
