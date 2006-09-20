982376d46edee94d76b17e613bd2f6677dd3ffb2
Dan Tran <dantran@gmail.com>
2006-09-20 05:52:17 +0000

    add dummy Javah provider for testing purpose
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/maven-native@2381 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ native-maven-plugin/src/test/java/org/codehaus/mojo/natives/noop/NoopJavah.java
@@ -0,0 +1,23 @@
+package org.codehaus.mojo.natives.noop;
+
+
+import org.codehaus.mojo.natives.NativeBuildException;
+import org.codehaus.mojo.natives.javah.Javah;
+import org.codehaus.mojo.natives.javah.JavahConfiguration;
+
+/**
+ * Helper class to test native-maven-plugin' javah mojo
+ * @author dtran
+ *
+ */
+public class NoopJavah
+    implements Javah 
+{
+
+    public void compile( JavahConfiguration config )
+        throws NativeBuildException
+    {
+
+    }
+
+}
