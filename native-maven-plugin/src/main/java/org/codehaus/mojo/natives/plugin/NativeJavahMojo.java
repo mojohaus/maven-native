99c8d3405fa3121a4e6f1983e7f426302c968f90
Dan Tran <dantran@gmail.com>
2009-12-22 20:09:26 +0000

    MOJO-1448: use project.getCompileArtifacts() to include only compile scope when searching for JNI interface
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/maven-native@11511 52ab4f32-60fc-0310-b215-8acea882cd1b

--- native-maven-plugin/src/main/java/org/codehaus/mojo/natives/plugin/NativeJavahMojo.java
+++ native-maven-plugin/src/main/java/org/codehaus/mojo/natives/plugin/NativeJavahMojo.java
@@ -2,26 +2,23 @@ package org.codehaus.mojo.natives.plugin;
 
 /*
  * The MIT License
- *
+ * 
  * Copyright (c) 2004, The Codehaus
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
  * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 import org.apache.maven.project.MavenProject;
@@ -43,7 +40,6 @@ import org.apache.bcel.classfile.Method;
 import java.io.IOException;
 import java.io.File;
 
-import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -80,7 +76,7 @@ public class NativeJavahMojo
     private String[] classNames;
 
     /**
-     * Path to javah executable, if present, it will overide the default one which bases on architecture type. See 'implementation' argument
+     * Path to javah executable, if present, it will override the default one which bases on architecture type. See 'implementation' argument
      * @parameter
      * @optional
      */
@@ -100,7 +96,7 @@ public class NativeJavahMojo
      * 
      */
     protected File workingDirectory;
-    
+
     /**
      * Generated native source files go here
      * @parameter default-value="${project.build.directory}/native/javah" 
@@ -131,7 +127,7 @@ public class NativeJavahMojo
     private JavahManager manager;
 
     /**
-     * For unittest only
+     * For unit test only
      */
     private JavahConfiguration config;
 
@@ -179,28 +175,30 @@ public class NativeJavahMojo
     {
         List list = new ArrayList();
 
-        Set artifacts = this.project.getDependencyArtifacts();
+        List artifacts = this.project.getCompileArtifacts();
 
-        for ( Iterator iter = artifacts.iterator(); iter.hasNext(); )
+        if ( artifacts != null )
         {
-            Artifact artifact = (Artifact) iter.next();
-            
-            //pick only jar files
-            if ( ! "jar".equals( artifact.getType() ) )
+
+            for ( Iterator iter = artifacts.iterator(); iter.hasNext(); )
             {
-                continue;	
+                Artifact artifact = (Artifact) iter.next();
+
+                //pick up only jar files
+                if ( !"jar".equals( artifact.getType() ) )
+                {
+                    continue;
+                }
+
+                //exclude some other scopes
+                if ( Artifact.SCOPE_PROVIDED.equals( artifact.getScope() ) )
+                {
+                    continue;
+                }
+
+                list.add( artifact );
+
             }
-            
-            //exclude some other scopes
-            if ( Artifact.SCOPE_PROVIDED.equals( artifact.getScope() ) ||
-                 Artifact.SCOPE_TEST.equals( artifact.getScope() )
-               )
-            {
-            	continue;
-            }
-            
-            list.add( artifact );
-            
         }
 
         return list;
@@ -289,7 +287,7 @@ public class NativeJavahMojo
             }
             catch ( IOException ioe )
             {
-                throw new MojoExecutionException( "Error searching for native class in " + artifact.getFile() , ioe );
+                throw new MojoExecutionException( "Error searching for native class in " + artifact.getFile(), ioe );
             }
         }
 
