a99213b3a6a5ed32b98b5de4c97b1483e6d57c3e
Dan Tran <dantran@gmail.com>
2013-12-20 23:54:27 +0000

    [MOJO-1730] more meaningful error message
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/maven-native@19078 52ab4f32-60fc-0310-b215-8acea882cd1b

--- maven-native-api/src/main/java/org/codehaus/mojo/natives/compiler/AbstractCompiler.java
+++ maven-native-api/src/main/java/org/codehaus/mojo/natives/compiler/AbstractCompiler.java
@@ -2,18 +2,18 @@ package org.codehaus.mojo.natives.compiler;
 
 /*
  * The MIT License
- * 
+ *
  * Copyright (c) 2004, The Codehaus
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
  * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -125,7 +125,6 @@ public abstract class AbstractCompiler
             }
             catch ( InterruptedException e )
             {
-
             }
 
             if ( compilerThreadPoolExecutor.isErrorFound() )
@@ -139,7 +138,7 @@ public abstract class AbstractCompiler
 
     /**
      * return "obj" or "o" when file extension is not given based on current platform
-     * 
+     *
      * @return
      */
     protected static String getObjectFileExtension( String fileExtension )
@@ -163,7 +162,7 @@ public abstract class AbstractCompiler
 
     /**
      * Figure out the object file relative path from a given source file
-     * 
+     *
      * @param sourceFile
      * @param workingDirectory
      * @param outputDirectory
@@ -195,7 +194,7 @@ public abstract class AbstractCompiler
         }
         catch ( IOException e )
         {
-            throw new NativeBuildException( e.getMessage() );
+            throw new NativeBuildException("Failed to figure out object file name for " + sourceFile + ": " + e.getMessage() , e );
         }
 
         File objectFile = new File( outputDirectory, objectFileName );
