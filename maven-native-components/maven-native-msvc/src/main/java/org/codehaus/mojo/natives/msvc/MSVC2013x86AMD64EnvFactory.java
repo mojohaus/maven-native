26772a9b9b14af19900a52da6206ac947b5c0049
Dan Tran <dantran@gmail.com>
2013-12-21 09:12:26 +0000

    [MOJO-1993] support VS2013
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/maven-native@19083 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ maven-native-components/maven-native-msvc/src/main/java/org/codehaus/mojo/natives/msvc/MSVC2013x86AMD64EnvFactory.java
@@ -0,0 +1,18 @@
+package org.codehaus.mojo.natives.msvc;
+
+
+import java.util.Map;
+
+import org.codehaus.mojo.natives.NativeBuildException;
+
+public class MSVC2013x86AMD64EnvFactory
+    extends AbstractMSVCEnvFactory
+{
+
+    protected Map createEnvs()
+        throws NativeBuildException
+    {
+        return this.createEnvs( "VS120COMNTOOLS", "amd64" );
+    }
+
+}
