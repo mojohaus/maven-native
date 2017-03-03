00d3d40e8fd23563f2235c7726368742a5e0729a
Guillaume Chauvet <gchauvet@zatarox.com>
2017-03-03 12:00:41 +0100

    Check generated output content

--- native-maven-plugin/src/test/java/org/codehaus/mojo/natives/plugin/NativeDumpMojoTest.java
+++ native-maven-plugin/src/test/java/org/codehaus/mojo/natives/plugin/NativeDumpMojoTest.java
@@ -1,7 +1,9 @@
 package org.codehaus.mojo.natives.plugin;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.HashMap;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 
@@ -21,5 +23,7 @@ public class NativeDumpMojoTest extends AbstractMojoTestCase {
         mojo.setPluginContext(new HashMap());
         mojo.execute();
 
+        assertEquals("080d283edcdbfaeb7ccf7f6a6cdeb071", DigestUtils.md5Hex(new FileInputStream(new File("target/generated-sources/c/dump.h"))));
+        assertEquals("4d50d16822d0eff1edd89f1b872975bc", DigestUtils.md5Hex(new FileInputStream(new File("target/generated-sources/c/dump.c"))));
     }
 }
